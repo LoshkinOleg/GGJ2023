989a50a3055e9c93117fabceee175e3b2b39f32b
IPuccini <puccini.ignacio@gmail.com>
2023-02-04 01:44:51 +0100

    Going back in the chain

--- Assets/Scripts/Root/Chain.cs
+++ Assets/Scripts/Root/Chain.cs
@@ -1,84 +1,143 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Chain : MonoBehaviour
 {
-    private struct PosAndRot
-    {
-        public Vector3 pos;
-        public Quaternion rot;
-    }
+	private struct PosAndRot
+	{
+		public Vector3 pos;
+		public Quaternion rot;
+	}
 
 
-    [SerializeField]
-    private List<ChainElement> _elements = null;
-    [SerializeField]
-    private ChainElement _chainPrefab = null;
+	[SerializeField]
+	private List<ChainElement> _elements = null;
+	[SerializeField]
+	private ChainElement _chainPrefab = null;
 
-    [SerializeField]
-    private Transform _head = null;
-    [SerializeField]
-    private float _distance = .5f;
+	[SerializeField]
+	private Transform _head = null;
+	[SerializeField]
+	private float _distance = .5f;
 
-    private float _timer = 0f;
+	[SerializeField]
+	private float _returnSpeed = 1f;
+	[SerializeField]
+	private float _returnChangeTarget = 1f;
 
-    private readonly Queue<PosAndRot> _headPos = new Queue<PosAndRot>();
-    private IPool<ChainElement> _poolChain = null;
 
-    private void Start()
-    {
-        _poolChain = PoolsManager.Instance.CreatePool("Chain", true, _chainPrefab);
-        _poolChain.GenerateAvailableInstances(100); // TODO number
-        AddChain();
+
+	private float _timer = 0f;
+
+	private readonly Queue<PosAndRot> _headPos = new Queue<PosAndRot>();
+	private IPool<ChainElement> _poolChain = null;
+
+
+	private bool _returning = false;
+	private int _returningCount = 0;
+
+
+	private void Start()
+	{
+		_poolChain = PoolsManager.Instance.CreatePool("Chain", true, _chainPrefab);
+		_poolChain.GenerateAvailableInstances(100); // TODO number
+
+		AddChain();
+
 
 	}
 
-    private void OnEnable()
-    {
-        // TODO change this number
-        for (int i = 0; i < 20; i++)
-        {
-            PosAndRot posAndRot = new PosAndRot();
-            posAndRot.pos = Vector3.zero;
-            posAndRot.rot = Quaternion.identity;
-            _headPos.Enqueue(posAndRot);
-        }
-    }
+	private void OnEnable()
+	{
+		// TODO change this number
+		for (int i = 0; i < 20; i++)
+		{
+			PosAndRot posAndRot = new PosAndRot();
+			posAndRot.pos = Vector3.zero;
+			posAndRot.rot = Quaternion.identity;
+			_headPos.Enqueue(posAndRot);
+		}
+	}
 
 
-    private void LateUpdate()
-    {
-        // this is not performant at all
-        PosAndRot posAndRot = new PosAndRot();
-        posAndRot.pos = _head.position;
-        posAndRot.rot = _head.rotation;
-        _headPos.Enqueue(posAndRot);
-        _headPos.Dequeue();
+	private void Update()
+	{
+		// This code is ugly :(
+		if (_returning)
+		{
+			if (_returningCount < _elements.Count -1)
+			{
+				_head.transform.position = Vector3.Lerp(_head.transform.position, _elements[_returningCount].transform.position, _returnSpeed * Time.deltaTime);
+			}
+			_timer -= Time.deltaTime;
+			if (_timer < 0f)
+			{
+				_returningCount++;
+				if (_returningCount >= _elements.Count -1)
+				{
 
-        _timer -= Time.deltaTime;
-        if (_timer < 0f)
-        {
-            for (int i = _elements.Count - 1; i > 0; i--)
-            {
-                _elements[i].transform.SetPositionAndRotation(_elements[i - 1].transform.position, _elements[i - 1].transform.rotation);
-            }
+					NewChain();
+				}
+				_timer = _returnChangeTarget;
+			}
+		}
+	}
 
-            _elements[0].transform.SetPositionAndRotation(_head.transform.position, _head.transform.rotation);
+	private void LateUpdate()
+	{
+		if (!_returning)
+		{
 
-            AddChain();
+			// this is not performant at all
+			PosAndRot posAndRot = new PosAndRot();
+			posAndRot.pos = _head.position;
+			posAndRot.rot = _head.rotation;
+			_headPos.Enqueue(posAndRot);
+			_headPos.Dequeue();
 
-			_timer = _distance;
-        }
-    }
+			_timer -= Time.deltaTime;
+			if (_timer < 0f)
+			{
+				for (int i = _elements.Count - 1; i > 0; i--)
+				{
+					_elements[i].transform.SetPositionAndRotation(_elements[i - 1].transform.position, _elements[i - 1].transform.rotation);
+				}
+
+				_elements[0].transform.SetPositionAndRotation(_head.transform.position, _head.transform.rotation);
+
+				AddChain();
+
+				_timer = _distance;
+			}
+		}
+	}
 
 
-    private void AddChain()
-    {
+	private void AddChain()
+	{
 
-        ChainElement newChainElement = _poolChain.GetInstance();
-        newChainElement.gameObject.SetActive(true);
+		ChainElement newChainElement = _poolChain.GetInstance();
+		newChainElement.gameObject.SetActive(true);
 		_elements.Add(newChainElement);
 	}
+
+
+
+
+
+	public void NewChain()
+	{
+		if (_returning)
+		{
+			_returningCount = 0;
+			_timer = 0f;
+			_elements.Clear();
+			AddChain();
+		}
+		_returning = !_returning;
+	}
+
 }
--- Assets/Scripts/Root/RootMovement.cs
+++ Assets/Scripts/Root/RootMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class RootMovement : MonoBehaviour
@@ -8,9 +9,12 @@ public class RootMovement : MonoBehaviour
 	[SerializeField]
 	private float _rotationSpeed = 40f;
 
+	public UnityEvent _onAction = null;
+
+
 	private InputActionsRoot _inputActions = null;
-	private Vector3 _moveDirection = Vector3.zero;
-	private Vector2 _input = Vector2.zero;
+	private Vector3 _moveDirection = Vector3.down;
+	private Vector2 _input = Vector2.down;
 
 	private void OnEnable()
 	{
@@ -20,16 +24,20 @@ public class RootMovement : MonoBehaviour
 		}
 		_inputActions.Gameplay.Enable();
 		_inputActions.Gameplay.Movement.performed += OnMovement;
+
+		_inputActions.Gameplay.Action.performed += OnAction;
 	}
 
 	private void OnDisable()
 	{
 		_inputActions.Gameplay.Movement.performed -= OnMovement;
+		_inputActions.Gameplay.Action.performed -= OnAction;
 	}
 
 	private void Update()
 	{
 		_moveDirection = Vector2.Lerp(_moveDirection, _input, _rotationSpeed * Time.deltaTime);
+		_moveDirection.Normalize();
 
 		Vector3 pos = transform.position;
 		pos += _speed * Time.deltaTime * _moveDirection;
@@ -40,11 +48,20 @@ public class RootMovement : MonoBehaviour
 
 	private void OnMovement(InputAction.CallbackContext obj)
 	{
-		_input = obj.ReadValue<Vector2>();
+		_input =   obj.ReadValue<Vector2>();
+		_input.y = Mathf.Floor(_input.y);
 		if (_input == Vector2.zero)
 		{
 			_input = _moveDirection;
 		}
+
+	}
+
+	private void OnAction(InputAction.CallbackContext obj)
+	{
+		// This is for debug. I need to change it
+		_onAction.Invoke();
+
 	}
 
 
