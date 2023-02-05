df0df32238cd79fbf23ee376af4c56923ca8fb9e
IPuccini <puccini.ignacio@gmail.com>
2023-02-05 09:14:44 +0100

    Project settings. Femur updated

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -149,7 +149,9 @@ public class GM : MonoBehaviour
 
     public void Quit()
     {
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+#endif
         Application.Quit();
     }
 
