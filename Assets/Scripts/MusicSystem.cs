c0fc7528a19f1a29b90e500fa8d7788aad328263
IPuccini <puccini.ignacio@gmail.com>
2023-02-05 11:09:09 +0100

    Music systen

--- Assets/Scripts/MusicSystem.cs
+++ Assets/Scripts/MusicSystem.cs
@@ -1,30 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MusicSystem : MonoBehaviour
 {
     [SerializeField] private LifeSystem _lifeSystem;
-    [SerializeField] AudioSource _musicLifeHighest;
-    [SerializeField] AudioSource _musicLifeHigh;
-    [SerializeField] AudioSource _musicLifeLow;
-    [SerializeField] AudioSource _musicLifeLowest;
     [SerializeField] float _timeForHighest = 60.0f;
-    [SerializeField] float _timeForHigh = 40.0f;
     [SerializeField] float _timeForLow = 25.0f;
     [SerializeField] float _timeForLowest = 10.0f;
 
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private AudioMixerSnapshot _menuSnapshot;
+    [SerializeField]
+    private AudioMixerSnapshot _startSnapshot;
+    [SerializeField]
+    private AudioMixerSnapshot _deshydrateSnapshot;
+    [SerializeField]
+    private AudioMixerSnapshot _deadSnapshot;
+
+    public enum Status
     {
-        
+        NONE,
+        MENU,
+        START,
+        DESHYDRATE,
+        DEAD
     }
 
-    // Update is called once per frame
-    void Update()
+    private Status _currentStatus = Status.NONE;
+
+    private void OnEnable()
     {
-        var remainingTime = _lifeSystem.CurrentLife;
-        
+        ChangeStatus(Status.MENU);
+    }
+
+    private void Update()
+    {
+        if(GM.Instance.Paused)
+        {
+            ChangeStatus(Status.MENU);
+            return;
+        }
+        if(_lifeSystem.CurrentLife >= _timeForHighest)
+        {
+            ChangeStatus(Status.START);
+        }else if(_lifeSystem.CurrentLife >= _timeForLow)
+        {
+            ChangeStatus(Status.DESHYDRATE);
+        }else if (_lifeSystem.CurrentLife <= _timeForLowest)
+        {
+            ChangeStatus(Status.DEAD);
+        }
+    }
+
+    public void ChangeStatus(Status status)
+    {
+        if(status == _currentStatus)
+        {
+            return;
+        }
+        _currentStatus = status;
+        switch (status)
+        {
+            case Status.MENU:
+                _menuSnapshot.TransitionTo(1);
+                break;
+            case Status.START:
+                _startSnapshot.TransitionTo(1);
+                break;
+            case Status.DESHYDRATE:
+                _deshydrateSnapshot.TransitionTo(1);
+                break;
+            case Status.DEAD:
+                _deadSnapshot.TransitionTo(1);
+                break;
+            default:
+                break;
+        }
     }
 }
